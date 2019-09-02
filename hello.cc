c3fad3c9160d9e3e9b052f1a36d78ab1a7cdef90
Rizk92 <rizke@unm.edu>
2019-09-02 10:26:58 -0600

    NEW UPDATE

--- hello.cc
+++ hello.cc
@@ -4,8 +4,9 @@ int main()
 {
 	std::cout << " # Eric Rizk " << std::endl;
         std::cout << " #### BIO:" << std::endl;
-        std::cout << " **From:** Tucson AZ,**Moved to** ABQ in 2015  "<< std::endl;
+        std::cout << " **From:** Tucson AZ, **Moved to** ABQ in 2015  "<< std::endl;
         std::cout << " **Major:** Computer Engineering " << std::endl;
+	std::cout<< " " << std::endl;
         std::cout << " ***Level of Programming:*** This will be my third programing class  " << std::endl;
         std::cout << " **Programming Languages:** ***Java and C*** " << std::endl;
         std::cout << " " << std::endl;
