62c6977d217747541e2958b9fd809c01a4312257
Eric Rizk <rizke@unm.edu>
2019-09-02 09:47:57 -0600

    Update Hellow.cc

--- Hellow.cc
+++ Hellow.cc
@@ -2,6 +2,14 @@
 
 int main()
 {
-	std::cout <<"Hello My Name is Eric"<< std::endl;
-	return 0;
+	std::cout << " # Eric Rizk " << std::endl;
+        std::cout << " #### BIO:" << std::endl;
+        std::cout << " **From:** Tucson AZ,**Moved to** ABQ in 2015  "<< std::endl;
+        std::cout << " **Major:** Computer Engineering " << std::endl;
+        std::cout << " ***Level of Programming:*** This will be my third programing class  " << $
+        std::cout << " **Programming Languages:** ***Java and C*** " << std::endl;
+        std::cout << " " << std::endl;
+        std::cout << " **ME:** ~~I like long walks on the beach~~, In my free time, I like like $
+        return 0;
+
 }
