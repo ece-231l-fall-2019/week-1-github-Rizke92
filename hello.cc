e863ba871c24bcc1cb6b9cb55683657df2c60bfd
Rizk92 <rizke@unm.edu>
2019-09-03 20:16:14 -0600

    New Updates

--- hello.cc
+++ hello.cc
@@ -3,13 +3,14 @@
 int main()
 {
 	std::cout << " # Eric Rizk " << std::endl;
-        std::cout << " #### BIO:" << std::endl;
-        std::cout << " **From:** Tucson AZ, **Moved to** ABQ in 2015  "<< std::endl;
+        std::cout << " ### BIO:" << std::endl;
+        std::cout << " **From:** Tucson Arizona, I moved to Albuquerque in 2015  "<< std::endl;
         std::cout << " **Major:** Computer Engineering " << std::endl;
 	std::cout<< " " << std::endl;
-        std::cout << " ***Level of Programming:*** This will be my third programing class  " << std::endl;
-        std::cout << " **Programming Languages:** ***Java and C*** " << std::endl;
+        std::cout << " ***Level of Programming:*** This will be my third programming class  " << std::endl;
+        std::cout << " **Programming Languages:** Java and C " << std::endl;
         std::cout << " " << std::endl;
-        std::cout << " **ME:** ~~I like long walks on the beach~~, In my free time, I game on the PS4. When I am not lazy, I like to go hiking." << std::endl;
+        std::cout << " **ME:** ~~I like long walks on the beach~~, In my free time, I game on the PS4. When I am not lazy, I like to go hiking with my dog, Beretta." << std::endl;
+	std::cout << "![Image of Beretta](Dog2.jpg)" << std::endl;
         return 0;
 }
--- hello.md
+++ hello.md
@@ -1,9 +1,10 @@
  # Eric Rizk 
- #### BIO:
- **From:** Tucson AZ, **Moved to** ABQ in 2015  
+ ### BIO:
+ **From:** Tucson Arizona, I moved to Albuquerque in 2015  
  **Major:** Computer Engineering 
  
- ***Level of Programming:*** This will be my third programing class  
- **Programming Languages:** ***Java and C*** 
+ ***Level of Programming:*** This will be my third programming class  
+ **Programming Languages:** Java and C 
  
- **ME:** ~~I like long walks on the beach~~, In my free time, I game on the PS4. When I am not lazy, I like to go hiking.
+ **ME:** ~~I like long walks on the beach~~, In my free time, I game on the PS4. When I am not lazy, I like to go hiking with my dog, Beretta.
+![Image of Beretta](Dog2.jpg)
